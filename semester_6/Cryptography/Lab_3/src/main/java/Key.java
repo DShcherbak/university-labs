98f9f6d2120d1be3d4ddfc4bf9d2a46a710bc991
DShcherbak <denys.shcherbak.official@gmail.com>
2021-06-07 14:40:48 +0300

    Implemented Lab_3 (DSA)

--- semester_6/Cryptography/Lab_3/src/main/java/Key.java
+++ semester_6/Cryptography/Lab_3/src/main/java/Key.java
@@ -1,41 +1,32 @@
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.PrintWriter;
 import java.math.BigInteger;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Created by Dmitrii on 17.11.2016.
- */
 public class Key {
     private ArrayList<BigInteger> Entity;
 
-    public Key(){
-        Entity = new ArrayList<BigInteger>();
-    }
-
     public Key(BigInteger[] keys){
-        Entity = new ArrayList<BigInteger>();
-        for (int i = 0; i < keys.length; i++){
-            Entity.add(keys[i]);
-        }
+        Entity = new ArrayList<>();
+        Entity.addAll(Arrays.asList(keys));
     }
     public Key(String path) throws FileNotFoundException {
         readFromFile(path);
     }
 
-    public BigInteger get(int idx){
-        if (idx >= Entity.size()) {
-            throw new IllegalArgumentException("idx more then size");
+    public BigInteger get(int index){
+        if (index >= Entity.size()) {
+            throw new IllegalArgumentException("Out of key bounds exception");
         }
-        return Entity.get(idx);
+        return Entity.get(index);
     }
 
     public void readFromFile(String path) throws FileNotFoundException {
         Scanner scanner = new Scanner(new FileReader(path));
-        Entity = new ArrayList<BigInteger>();
+        Entity = new ArrayList<>();
         while (scanner.hasNextBigInteger()){
             Entity.add(scanner.nextBigInteger());
         }
@@ -43,7 +34,7 @@ public class Key {
     }
 
     public void writeToFile(String path) throws FileNotFoundException {
-        PrintWriter printWriter = new PrintWriter(new File(path));
+        PrintWriter printWriter = new PrintWriter(path);
         for (BigInteger element : Entity){
             printWriter.println(element);
         }
--- semester_6/Cryptography/Lab_3/src/main/java/Main.java
+++ semester_6/Cryptography/Lab_3/src/main/java/Main.java
@@ -1,25 +1,23 @@
-import java.io.*;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.SecureRandom;
-import java.util.Arrays;
+import java.security.spec.ECField;
+import java.util.Scanner;
+import java.security.*;
 
-public class Signature {
-
-    static BigInteger one = new BigInteger("1");
-    static BigInteger two = new BigInteger("2");
-
+public class Main {
+    static String pathSign = "Sign.txt";
+    static String pathPublicKey = "PublicKey.txt";
+    static String pathPrivateKey = "PrivateKey.txt";
 
     public static boolean checkSign(String path, String pathPublicKey, String pathSign) throws IOException, NoSuchAlgorithmException {
         System.out.println("checking sign");
         Key PublicKey = new Key(pathPublicKey);
         Key Sign = new Key(pathSign);
-        BigInteger q = PublicKey.get(0);
-        BigInteger p = PublicKey.get(1);
+        BigInteger p = PublicKey.get(0);
+        BigInteger q = PublicKey.get(1);
         BigInteger g = PublicKey.get(2);
         BigInteger y = PublicKey.get(3);
         BigInteger r = Sign.get(0);
@@ -30,23 +28,25 @@ public class Signature {
         byte[] digest = md5.digest( );
         BigInteger Hm = new BigInteger( 1, digest );
 
-        // Compute w, such that s*w mod q = 1. w is called the modular multiplicative inverse of s modulo q
+        // w = s^-1 mod q
         BigInteger w = s.modInverse(q);
 
-        // Compute u1 = H(m)*w mod q
+        // u1 = H(m)*w mod q
         BigInteger u1 = Hm.multiply(w).mod(q);
 
-        // Compute u2 = r*w mod q
+        // u2 = r*w mod q
         BigInteger u2 = r.multiply(w).mod(q);
 
-        // Compute v = (((g^u1)*(y^u2)) mod p) mod q
-        BigInteger v = g.mod(p).modPow(u1, p).multiply(y.mod(p).modPow(u2, p)).mod(p).mod(q);
+        // v = (((g^u1)*(y^u2)) mod p) mod q
+        BigInteger gu1 = g.modPow(u1, p);
+        BigInteger yu2 = y.modPow(u2, p);
+        BigInteger v = gu1.multiply(yu2).mod(p).mod(q);
 
         if (r.equals(v)) {
-            System.out.println("Schnorr signature is valid");
+            System.out.println("DSA is valid");
             return true;
         } else {
-            System.out.println("Schnorr signature is not valid");
+            System.out.println("DSA is not valid");
             return false;
         }
     }
@@ -54,18 +54,16 @@ public class Signature {
     public static void makeSign(String path, String pathPublicKey, String pathPrivateKey, String pathSign) throws IOException, NoSuchAlgorithmException {
         Key PublicKey = new Key(pathPublicKey);
         Key PrivateKey = new Key(pathPrivateKey);
-        BigInteger p = PublicKey.get(1);
-        BigInteger q = PublicKey.get(0);
+        BigInteger p = PublicKey.get(0);
+        BigInteger q = PublicKey.get(1);
         BigInteger g = PublicKey.get(2);
-        BigInteger x = PublicKey.get(3);
-        BigInteger y = PrivateKey.get(0);
+        BigInteger x = PrivateKey.get(0);
 
         SecureRandom sr = new SecureRandom();
 
-
         BigInteger k = new BigInteger(q.bitLength(), sr).mod(q).add(BigInteger.ONE);
-        BigInteger r = g.modPow(k, p).mod(q);
-        BigInteger notK = k.modInverse(q);
+        BigInteger r;
+        BigInteger notK;
 
         MessageDigest md5 = MessageDigest.getInstance("MD5");
         md5.update(Files.readAllBytes(Paths.get(path)));
@@ -75,45 +73,37 @@ public class Signature {
         do
         {
             k = new BigInteger(160, new SecureRandom()).mod(q.subtract(BigInteger.ONE)).add(BigInteger.ONE);
-
+            notK = k.modInverse(q);
             r = g.modPow(k, p).mod(q);
-
             if (r.compareTo(BigInteger.ZERO) == 0)
                 continue;
 
-
             s = notK.multiply(Hm.add(r.multiply(x))).mod(q);
-
             if (s.compareTo(BigInteger.ZERO) == 0)
                 continue;
-
             break;
         }
         while (true);
 
         Key Sign = new Key(new BigInteger[]{r, s});
         Sign.writeToFile(pathSign);
-        System.out.println("Success!");
+        System.out.println("Signing complete!");
     }
 
     public static void generate(int bitLength, String pathPublicKey, String pathPrivateKey) throws FileNotFoundException {
-        System.out.println("generating:");
-
-
-        BigInteger q, p, a, g, x, y;
         int certainty = 100;
         SecureRandom sr = new SecureRandom();
-        q = new BigInteger(bitLength, certainty, sr);
+        BigInteger q = new BigInteger(bitLength, certainty, sr);
 
-        p = generateP(q);
+        BigInteger p = generateP(q);
 
-        g = generateG(bitLength, p, q);
+        BigInteger g = generateG(bitLength, p, q);
 
-        x = new BigInteger(bitLength, sr).modPow(BigInteger.ONE, q);
+        BigInteger x = new BigInteger(bitLength, sr).mod(q);
 
-        y = g.modPow(x, p);
+        BigInteger y = g.modPow(x, p);
 
-        Key PublicKey = new Key(new BigInteger[]{q, p, g, y});
+        Key PublicKey = new Key(new BigInteger[]{p,q, g, y});
         PublicKey.writeToFile(pathPublicKey);
         System.out.println("general public info : (");
         System.out.println(p);
@@ -133,7 +123,7 @@ public class Signature {
         BigInteger k, g;
         SecureRandom sr = new SecureRandom();
         while (true) {
-            k = (two.add(new BigInteger(bitLength, 100, sr))).mod(p);
+            k = BigInteger.TWO.add(new BigInteger(bitLength, 100, sr)).mod(p);
             BigInteger power = (p.subtract(BigInteger.ONE)).divide(q);
             g = k.modPow(power, p);
             if (g.compareTo(BigInteger.ONE) != 0)
@@ -145,10 +135,20 @@ public class Signature {
         BigInteger qp = BigInteger.ONE;
         BigInteger p;
         while (true){
-            p = q.multiply(qp).multiply(two).add(one);
+            p = q.multiply(qp).multiply(BigInteger.TWO).add(BigInteger.ONE);
             if (p.isProbablePrime(100))
                 return p;
-            qp = qp.add(one);
+            qp = qp.add(BigInteger.ONE);
         }
+    }
+
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
+
+        String fileToSign = "Signed information.txt";
+        int bitSize = 4;
+
+        generate(bitSize, pathPublicKey, pathPrivateKey);
+        makeSign(fileToSign, pathPublicKey, pathPrivateKey, pathSign);
+        checkSign(fileToSign, pathPublicKey, pathSign);
     }
 }
